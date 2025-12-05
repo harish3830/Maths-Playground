bf4ca162f309e005c1e6707193ac0aa1f9686a81
harish3830 <harish.hks2000@gmail.com>
2025-12-05 10:24:58 +0530

    Updated project with new changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./Pages/Home";
-import MeanCalculator from "./components/mean";
 
 export default function App() {
   return (
